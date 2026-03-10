3e5936ca9b8215a3adcaf71b9f2251f7e1a7401b
saiteja <saitejavurukonda5@gmail.com>
2026-03-10 21:27:23 +0530

    Added Guess Number Java Game

---
+++ GuessGame.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class GuessGame {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int number = rand.nextInt(100) + 1;
+        
+System.out.println("Secret number: " + number);
+
+        while (true) {
+
+            System.out.print("Enter your guess: ");
+            int guess = sc.nextInt();
+
+            int diff = Math.abs(number - guess);
+
+            if (guess == number) {
+                System.out.println("Correct! You guessed the number.");
+                break;
+            }
+            else if (diff <= 10) {
+                System.out.println("Hot");
+            }
+            else {
+                System.out.println("Cold");
+            }
+
+        }
+
+        sc.close();
+    }
+}
